1942cff2a91f298e38c520d6185ec6030a267b4c
projectstein <projectstein@gmail.com>
2018-07-05 23:39:26 -0300

    Tratamneto de exceção

---
+++ src/main/java/com/projectstein/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.projectstein.cursomc.resources.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.projectstein.cursomc.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandartError> objectnotfound(ObjectNotFoundException e, HttpServletRequest request){
+		StandartError erro = new StandartError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+	}
+
+}
--- src/main/java/com/projectstein/cursomc/services/CategoriaService.java
+++ src/main/java/com/projectstein/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.projectstein.cursomc.domain.Categoria;
 import com.projectstein.cursomc.repository.CategoriaRepository;
+import com.projectstein.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,11 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+			if(obj == null) {
+				throw new ObjectNotFoundException("Objeto não encontrado! id: "+ 
+														id +" Tipo: "+ Categoria.class.getName());
+			}
+		
 		return obj;
 	}
 	
