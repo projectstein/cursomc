a3d58e8f477ac6971b8ff1993b0a023acd672cbe
projectstein <projectstein@gmail.com>
2018-07-26 20:34:44 -0300

    DELETE Categoria

--- src/main/java/com/projectstein/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/projectstein/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.projectstein.cursomc.services.exception.DataIntegrityException;
 import com.projectstein.cursomc.services.exception.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -14,8 +15,16 @@ public class ResourceExceptionHandler {
 	
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandartError> objectnotfound(ObjectNotFoundException e, HttpServletRequest request){
+		
 		StandartError erro = new StandartError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
 	}
 
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandartError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
+		
+		StandartError erro = new StandartError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+	}
 }
--- src/main/java/com/projectstein/cursomc/services/CategoriaService.java
+++ src/main/java/com/projectstein/cursomc/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package com.projectstein.cursomc.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.projectstein.cursomc.domain.Categoria;
 import com.projectstein.cursomc.repository.CategoriaRepository;
+import com.projectstein.cursomc.services.exception.DataIntegrityException;
 import com.projectstein.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -33,6 +35,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.delete(id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível deletar essa categoria pois ela tem produtos");	
+		}
 	}
 	
 
