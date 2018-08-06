b4b459721fc37dfc5bd8ec7b8ec5700f165df7a7
projectstein <projectstein@gmail.com>
2018-08-06 20:54:54 -0300

    Validação update Email Cliente

--- src/main/java/com/projectstein/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/projectstein/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.projectstein.cursomc.domain.Cliente;
+import com.projectstein.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
