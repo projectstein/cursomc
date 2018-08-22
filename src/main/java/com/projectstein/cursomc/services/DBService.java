881b95eb784c0fe414383347f7bfa803fa57d85e
projectstein <projectstein@gmail.com>
2018-08-22 14:19:15 -0300

    Salvando perfis de usuário na base de dados

--- src/main/java/com/projectstein/cursomc/services/DBService.java
+++ src/main/java/com/projectstein/cursomc/services/DBService.java
@@ -20,6 +20,7 @@ import com.projectstein.cursomc.domain.PagamentoComCartao;
 import com.projectstein.cursomc.domain.Pedido;
 import com.projectstein.cursomc.domain.Produto;
 import com.projectstein.cursomc.domain.enums.EstadoPagamento;
+import com.projectstein.cursomc.domain.enums.Perfil;
 import com.projectstein.cursomc.domain.enums.TipoCliente;
 import com.projectstein.cursomc.repository.CategoriaRepository;
 import com.projectstein.cursomc.repository.CidadeRepository;
@@ -112,14 +113,23 @@ public class DBService {
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "suportearrecadacao@projetatecnologia.com.br", "36378912377", TipoCliente.PESSOAFISICA,pe.encode("123"));
 		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "suportearrecadacao@projetatecnologia.com.br", "36378912377", TipoCliente.PESSOAFISICA,pe.encode("123"));
 		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
+		
+		Cliente cli2 = new Cliente(null, "Ana Costa", "projectstein@gmail.com", "82728337004", TipoCliente.PESSOAFISICA,pe.encode("123"));
+		cli2.addPerfil(Perfil.ADMIN);
+		cli2.getTelefones().addAll(Arrays.asList("27353323","93839393"));
+		
+		
 		
 		Endereco e1= new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834",cli1,c1);
 		Endereco e2= new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38220834",cli1,c2);
-	
+		Endereco e3= new Endereco(null, "Avenida Floriano", "2106", null, "Centro", "29220834",cli2,c2);
+		
+		
 		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		cli2.getEnderecos().addAll(Arrays.asList(e3));
 	
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
 		
@@ -148,8 +158,8 @@ public class DBService {
 		
 		estadoRepository.save(Arrays.asList(est1, est2));
 		cidadeRepository.save(Arrays.asList(c1,c2,c3));
-		clienteRepository.save(Arrays.asList(cli1));
-		enderecoRepository.save(Arrays.asList(e1,e2));
+		clienteRepository.save(Arrays.asList(cli1,cli2));
+		enderecoRepository.save(Arrays.asList(e1,e2,e3));
 		pedidoRepository.save(Arrays.asList(ped1,ped2));
 		pagamentoRepository.save(Arrays.asList(pagto1,pagto2));
 		itemPedidoRepository.save(Arrays.asList(ip1,ip2,ip3));
